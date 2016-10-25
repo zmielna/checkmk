923be8a0c294f63a7bb22377388039048eab9899
zmielna <zmielna@users.noreply.github.com>
2016-10-25 10:20:42 +0100

    Fixed displaying value for ZFS_CACHE in GB

--- feeding_zfs_stats_to_checkmk/zfs-log-cache-stats.sh
+++ feeding_zfs_stats_to_checkmk/zfs-log-cache-stats.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ZFSLOG=`zpool iostat -v |grep ada1p1|cut -d" " -f11`
-ZFSCACHE=`zpool iostat -v |grep ada1p2|cut -d" " -f11`
+ZFSCACHE=`zpool iostat -v |grep cache0|cut -d" " -f7`
 #ZFSLOG=3.00G
 #ZFSCACHE=5.00G
 
@@ -21,12 +21,12 @@ esac
 
 echo $Message
 
-# ------------------------- cache
+# ------------------------- cache 
 case $ZFSCACHE in
 *M)
   ZFSCACHEm=`echo $ZFSCACHE|sed 's/.$//'`
   ZFSCACHEg=`echo "$ZFSCACHEm*0.001"| bc`
-  Message="0 ZFS_CACHE ZFSCACHE_Size=$ZFSCACHE;60;65 Current size of used ZFS cachae device is $ZFSCACHE/65GB"
+  Message="0 ZFS_CACHE ZFSCACHE_Size=$ZFSCACHEg;60;65 Current size of used ZFS cachae device is $ZFSCACHE/65GB"
   ;;
 *G)
   ZFSCACHEm=`echo $ZFSCACHE|sed 's/.$//'`
